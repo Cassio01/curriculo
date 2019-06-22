3f3b8eb859c806020eb2a30286b97170aab5d502
Cassio Alves <cassio01>
2019-06-22 16:10:58 -0300

    melhorando o caminho da rota raiz e criando a rota curriculo

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const port = 3000;
+const path = require('path');
+
+const indexRoute = require('./routes/index');
+const curriculoRoute = require('./routes/curriculo');
+
 const app = express();
 
-app.get('/', (req,res,next) => {
-    res.send({
-        title: 'Meu primeiro servidor Express',
-        version: '0.0.0'
-    });
-});
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.get('/',indexRoute);
+app.get('/curriculo', curriculoRoute);
 
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`);
